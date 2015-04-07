8b433d3b7dc1f4f1e8189561d9de969035e54d47
Sadaf Matinkhoo <sadaf.matinkhoo@gmail.com>
2015-04-06 22:04:20 -0400

    part D finished

--- pi/partD/BugDetectionD.java
+++ pi/partD/BugDetectionD.java
@@ -37,22 +37,24 @@ To find more bugs, the order of functions being called is taken into account whe
  To use this algorithm, the 5th argument has to be 2.
  */
 
+class Config {
+    public static int algorithm = 1; // 1 for reducing false positives, 2 for finding more bugs
+}
 
-
+    
 public class BugDetectionD {
 	
     private String callgraph;
     private int support;
     private int confidence;
     private int level; // level of expansion for inter-procedural analysis. level = 0 implies intra-procedural analysis. 
-    private int algorithm; // 1 for reducing false positives, 2 for finding more bugs
     
  // A hash table with function names as keys and a hash set of nodes where the function have been  called as values.
     private Hashtable<String, HashSet<String>> functionsToNodesTable = new Hashtable<String, HashSet<String>>();
  // A hash table with pairs of function names as keys and a hash set of nodes where the function pair have been  called as values.    
     private Hashtable<Pair, HashSet<String>> functionsPairsToNodesTable = new Hashtable<Pair, HashSet<String>>();
  // A hash table with node names as keys and a hash set of functions that are called in the node as values. 
-    private Hashtable<String, HashSet<String>> nodesToFunctionsTable = new Hashtable<String, HashSet<String>>();
+    private Hashtable<String, ArrayList<String>> nodesToFunctionsTable = new Hashtable<String, ArrayList<String>>();
 
 //  IMPORTANT NOTE: We assume all 5 arguments passed for this partD of the project, 
 //  as it is hard to figure out otherwise which algorithm is to be used.
@@ -66,10 +68,8 @@ public class BugDetectionD {
         support = Integer.parseInt(args[1]);
         confidence = Integer.parseInt(args[2]);
         level = Integer.parseInt(args[3]);
-        algorithm = Integer.parseInt(args[4]);                       
+        Config.algorithm = Integer.parseInt(args[4]);                       
     }
-       
-    
     
 //  This function uses LLVM to generate a call graph, then parses each line to get 
 //  the functions that are called in each node, and stores them in the 
@@ -79,6 +79,7 @@ public class BugDetectionD {
         String callGraphLine = null;
         String nodeName = "";
         HashSet<String> functionsInNode = new HashSet<String>();
+	ArrayList<String> orderedFunctionsInNode = new ArrayList<String>();
         State state = State.IGNORE_FIRST_FEW_LINES;
         int lineNum=0;
         try {
@@ -90,7 +91,6 @@ public class BugDetectionD {
                     //to ignore the warnings in the beginning
                     case IGNORE_FIRST_FEW_LINES:
                         if (lineNum == 7) state = State.WAIT_FOR_FIRST_EMPTY_LINE; 
-                        //else System.out.println(callGraphLine);
                         break;
                     // callGraphLines within the first <null function> node are ignored    
                     case WAIT_FOR_FIRST_EMPTY_LINE:
@@ -105,18 +105,21 @@ public class BugDetectionD {
                         
                     case SAVE_FUNCTIONS:
                         if (callGraphLine.isEmpty()) { // this node is now done being parsed
-                            nodesToFunctionsTable.put(nodeName, new HashSet<String>(functionsInNode));
+                            nodesToFunctionsTable.put(nodeName, new ArrayList<String>(orderedFunctionsInNode));
                             functionsInNode.clear();
+                            orderedFunctionsInNode.clear();
                             state = State.SAVE_NODE_NAME;
                             break;
                         }
                         String[] functionName = callGraphLine.split("'");
                         if (functionName.length < 2) break; // to ignore the external nodes
-                        functionsInNode.add(functionName[1]);
+                        if (!functionsInNode.contains(functionName[1])) {
+				functionsInNode.add(functionName[1]);
+				orderedFunctionsInNode.add(functionName[1]);
+			}
                         break;
                 }
             } // end of while (finished reading callgraph)
-            processOutput.close();
         } catch (Exception ex) {
             System.err.println("Error: Error encountered in parsing call graph: " + ex.getMessage());
             ex.printStackTrace();
@@ -130,14 +133,15 @@ public class BugDetectionD {
  // the nodes contained in each node to the desired level of expansion.
  // Then, fills the functionsToNodesTable and functionPairsToNodesTable.
     private void createDataStructure () {
-    	Set<String> keySet = nodesToFunctionsTable.keySet();
+	Set<String> keySet = nodesToFunctionsTable.keySet();
     	Iterator<String> keySetIterator = keySet.iterator();
     	while (keySetIterator.hasNext()) {
     		String nodeName = keySetIterator.next();
-    		HashSet<String> functionsInNode = new HashSet<String>();
+		ArrayList<String> functionsInNode = new ArrayList<String>();
+		HashSet<String> added = new HashSet<String>();
     		if (!nodesToFunctionsTable.get(nodeName).isEmpty()) { 
 			Set<String> path = new HashSet<String>();
-			getFunctionCalls(nodeName, level, functionsInNode, path);
+			getFunctionCalls(nodeName, level, functionsInNode, added, path);
 		}
     		insertFunctionsPairsToNodes(functionsInNode, nodeName);
     		insertFunctionsToNodes(functionsInNode, nodeName);
@@ -149,18 +153,19 @@ public class BugDetectionD {
   // It takes the scope name as an argument, retrieves the values for that scope, which is
   // a set of function names, then iterates over those functions and retrives the functions called in each of them
   // from the nodesToFunctionsTable, and adds them to the functionsInNode hash set.
-    private void getFunctionCalls(String nodeName, int level, HashSet<String> functionsInNode, Set<String> path) {
+    private void getFunctionCalls(String nodeName, int level, ArrayList<String> functionsInNode, Set<String> added, Set<String> path) {
     	if (path.contains(nodeName)) return; // Avoid loops.
 	Iterator<String> iter = nodesToFunctionsTable.get(nodeName).iterator();
-    	if (!iter.hasNext() || level < 0)  {
-		// Only add leaf nodes. 
+    	if ((!iter.hasNext() || level < 0) && !added.contains(nodeName))  {
+		// Only add leaf nodes.
     		functionsInNode.add(nodeName);
+		added.add(nodeName);
 		return;
 	}
 	path.add(nodeName);
     	while (iter.hasNext()) {
     		String function = iter.next();
-    		getFunctionCalls(function, level-1, functionsInNode, path);
+    		getFunctionCalls(function, level-1, functionsInNode, added, path);
     	}
 	path.remove(nodeName);
     }
@@ -168,25 +173,23 @@ public class BugDetectionD {
 //  Inserts into hash table 'functionsPairsToNodesTable' where function pairs within a node are the keys,
 //  and the nodes its being called in are the values
 //  This function also creates the pairs from the set of functions calls within the node
-    private void insertFunctionsPairsToNodes(HashSet<String> set, String nodeName){
-        List<String> functions = new ArrayList<String>(set);
+    private void insertFunctionsPairsToNodes(ArrayList<String> set, String nodeName){
+	List<String> functions = new ArrayList<String>(set);
         for (int i = 0; i< functions.size(); i++){
             for (int j = i+1; j< functions.size(); j++){
                 Pair tempPair = new Pair(functions.get(i), functions.get(j));
                 if (! functionsPairsToNodesTable.containsKey(tempPair)){
-                    functionsPairsToNodesTable.put(tempPair, new HashSet<String>());
+		    functionsPairsToNodesTable.put(tempPair, new HashSet<String>());
                 } 
                 functionsPairsToNodesTable.get(tempPair).add(nodeName); 
             }
         }
     }
     
-    
 //  Inserts into hash table 'functionsToNodesTable' where function names are they keys,
 //  and the nodes its being called in are the values
-    private void insertFunctionsToNodes (HashSet<String> set, String nodeName) {
-        List<String> functions = new ArrayList<String>(set);
-        for (int i = 0; i< functions.size(); i++){
+    private void insertFunctionsToNodes (ArrayList<String> functions, String nodeName) {
+        for (int i = 0; i < functions.size(); i++){
             if (! functionsToNodesTable.containsKey(functions.get(i))){
                 functionsToNodesTable.put(functions.get(i), new HashSet<String>());
             } 
@@ -194,17 +197,15 @@ public class BugDetectionD {
         } 
     }
     
-    
 //  State machine used for parsing the call graph
     private enum State {
         IGNORE_FIRST_FEW_LINES, WAIT_FOR_FIRST_EMPTY_LINE, SAVE_NODE_NAME, SAVE_FUNCTIONS
     }
-    
-    
- // Class Pair defined, used for function pairs
-    private static class Pair {
-        private final String left;
-        private final String right;
+
+//  Class Pair defined, used for function pairs
+    private class Pair {
+        protected final String left;
+        protected final String right;
 
         public Pair (String left, String right) {
             this.left = left;
@@ -221,21 +222,48 @@ public class BugDetectionD {
 
         @Override
         public int hashCode() {
-            return left.hashCode() ^ right.hashCode();
+            return left.hashCode() ^ right.hashCode() ;
         }
 
         @Override
         public boolean equals(Object o) {
             if (!(o instanceof Pair)) return false;
             Pair pairo = (Pair) o;
-            return (this.left.equals(pairo.getLeft()) && this.right.equals(pairo.getRight())) || (this.right.equals(pairo.getLeft()) && this.left.equals(pairo.getRight()));
+	    return Config.algorithm == 1 ? (this.left.equals(pairo.getLeft()) && this.right.equals(pairo.getRight())) || 
+                (this.right.equals(pairo.getLeft()) && this.left.equals(pairo.getRight())) : 
+		(this.left.equals(pairo.getLeft()) && this.right.equals(pairo.getRight())); 
         }
         
         @Override
-        public String toString(){
-            return "("+ left + "," + right + ")";
+        public String toString() {
+	    return Config.algorithm == 1 ? ((left.compareTo(right) < 0 ? "(" + left + ", " + right + ")" : 
+	        "(" + right + ", " + left + ")")) : 
+		"(" + left + ", " + right + ")"; 
         }
-        
+	
+	public Pair dual() {
+		return new Pair(right, left);
+	}
+    }
+
+    private void findOpBugs() {
+    	// Iterate over the keys in functionPairsToNodesTable
+    	Iterator<Pair> iter = functionsPairsToNodesTable.keySet().iterator();
+	Set<Pair> processed = new HashSet<Pair>();
+    	while (iter.hasNext()) {
+		Pair keyPair = iter.next();
+    		Pair dualPair = keyPair.dual();
+		if (processed.contains(keyPair) || processed.contains(dualPair)) continue;
+		processed.add(keyPair);
+		if (functionsPairsToNodesTable.get(dualPair) == null) continue;
+		int support = functionsPairsToNodesTable.get(keyPair).size();
+		int dualSupport = functionsPairsToNodesTable.get(dualPair).size();
+    		if (dualSupport > support) {
+			findOpBugs(dualPair, keyPair, dualSupport, support);
+		} else { 
+			findOpBugs(keyPair, dualPair, support, dualSupport);
+		}
+    	}
     }
     
     private void findBugs() {
@@ -254,46 +282,58 @@ public class BugDetectionD {
     	}
     }
     
+    private void reportBugs(HashSet<String> functionScopes, HashSet<String> functionPairScopes, 
+         String function, Pair pair, int pairSupport, float bugConfidence) {
+       Iterator<String> iter = functionScopes.iterator();
+       while (iter.hasNext()) {
+          String scope = iter.next();
+    	  if (functionPairScopes.contains(scope)) continue;
+    	  // this is one scope where the function is called but the function pair
+    	  // is not called --> this is the scope where the bug happens
+    	  String bugReport = String.format("bug: %s in %s, pair: %s, "
+    	    				   + "support: %d, confidence: %.2f%%\n", function, scope,
+    	    				   pair, pairSupport, bugConfidence);
+    	  System.out.print(bugReport);
+       }
+    }
+    
     private void findBugs(String function, Pair keyPair, int pairSupport) {
     	HashSet<String> functionScopes = functionsToNodesTable.get(function);
     	HashSet<String> functionPairScopes = functionsPairsToNodesTable.get(keyPair);
     	int functionSupport = functionScopes.size();
     	// If pairSupport and functionSupport are equal, it means that the two functions in 
     	// the pair are always called together --> no bug!
-    	if (functionSupport > pairSupport) {
-    		float bugConfidence = pairSupport / ((float)functionSupport) * 100;
-    		if ((bugConfidence >= confidence && algorithm == 2)||(bugConfidence >=findConfidenceThreshold(pairSupport) && algorithm ==1)) {
-    			// if confidence is greater than or equal to the threshold
-    			// --> there is a bug! Print it!
-    			Iterator<String> iter = functionScopes.iterator();
-    	    	while (iter.hasNext()) {
-    	    		String scope = iter.next();
-    	    		if (!functionPairScopes.contains(scope)) {
-    	    			// this is one scope where the function is called but the function pair
-    	    			// is not called --> this is the scope where the bug happens
-    	    			
-    	    			// Sort the functions in pair and add them to the bug report
-    	    			String leftFunction = keyPair.getLeft();
-    	    			String rightFunction = keyPair.getRight();
-    	    			int compare = leftFunction.compareTo(rightFunction);
-    	    			String firstFunctionInPair, secondFunctionInPair;
-    	    			if (compare <= 0){
-    	    				firstFunctionInPair = keyPair.getLeft();
-    	    				secondFunctionInPair = keyPair.getRight();
-    	    			} else {
-    	    				firstFunctionInPair = keyPair.getRight();
-    	    				secondFunctionInPair = keyPair.getLeft();
-    	    			}
-    	    			String bugReport = String.format("bug: %s in %s, pair: (%s, %s), "
-    	    					+ "support: %d, confidence: %.2f%%\n", function, scope,
-    	    					firstFunctionInPair, secondFunctionInPair, pairSupport, bugConfidence);
-    	    			System.out.print(bugReport);
-    	    		}
-    	    	}
-    		}
+	if (functionSupport == pairSupport) return;
+        float bugConfidence = pairSupport / ((float)functionSupport) * 100;
+    	if ((bugConfidence >= confidence && Config.algorithm == 2) || (bugConfidence >=findConfidenceThreshold(pairSupport) && Config.algorithm ==1)) {
+    	   // if confidence is greater than or equal to the threshold
+    	   // --> there is a bug! Print it!
+	   reportBugs(functionScopes, functionPairScopes, function, keyPair, pairSupport, bugConfidence);
     	}
     }
-    
+   
+    private void reportOpBugs(Pair basePair, Pair pair, int baseSupport, float bugConfidence) {
+       HashSet<String> baseScopes = functionsPairsToNodesTable.get(basePair);
+       HashSet<String> scopes = functionsPairsToNodesTable.get(pair);
+       Iterator<String> iter = scopes.iterator();
+       while (iter.hasNext()) {
+          String scope = iter.next();
+    	  if (baseScopes.contains(scope)) continue;
+    	  String bugReport = String.format("bug: pair: %s in %s has a wrong order. "
+    	    				   + "support: %d, confidence: %.2f%%\n",
+    	    				   pair, scope, baseSupport, bugConfidence);
+    	  System.out.print(bugReport);
+       }
+    }
+
+    private void findOpBugs(Pair basePair, Pair pair, int baseSupport, int support) {
+    	if (baseSupport == support) return;
+	float bugConfidence = baseSupport / ((float)baseSupport + support) * 100;
+	if (bugConfidence >= confidence) {
+	   reportOpBugs(basePair, pair, baseSupport, bugConfidence);
+	}
+    }
+ 
     //Calculate confidence threshold 
     private int findConfidenceThreshold(int functionPairSupport){
         int confidenceThreshold = ((functionPairSupport - support)*(99-confidence)/(100 - support))+confidence;
@@ -304,9 +344,12 @@ public class BugDetectionD {
     public static void main(String[] args) {    
         BugDetectionD tool = new BugDetectionD();
         tool.parseArgs(args);
-        tool.parseCallGraph();
+ 	tool.parseCallGraph();
         tool.createDataStructure();
-    	tool.findBugs();
+    	if (Config.algorithm == 1) {
+	   tool.findBugs();
+	} else {
+	   tool.findOpBugs();
+    	}
     }
-
 }
